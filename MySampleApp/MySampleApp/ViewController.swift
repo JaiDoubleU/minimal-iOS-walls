844c2e503e737fb88e718d74a2870c899b626415
Alex Núñez <atb1983@gmail.com>
2019-06-07 05:54:20 +0100

    Initial commit

---
+++ MySampleApp/MySampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySampleApp
+//
+//  Created by Alex Núñez on 07/06/2019.
+//  Copyright © 2019 Touchsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
