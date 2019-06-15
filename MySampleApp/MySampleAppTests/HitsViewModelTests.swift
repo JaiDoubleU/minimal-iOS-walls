deb1cd617b6c5f06e14abf303437551a50c2249f
Alex Núñez <atb1983@gmail.com>
2019-06-15 14:09:56 +0100

    HitsViewModelTests

--- MySampleApp/MySampleAppTests/HitsViewModelTests.swift
+++ MySampleApp/MySampleAppTests/HitsViewModelTests.swift
@@ -18,12 +18,14 @@ class HitsViewModelTests: XCTestCase {
 	var networkManager: NetworkManager!
 	var pixaBayNetworkService: PixabayNetworkService!
 	var hitsIterator: HitsInteractor!
+	var mockJson: Data?
 	var coordinator: MockCoordinator!
 	var hitsViewModel: HitsViewModel!
 	
 	override func setUp() {
 		super.setUp()
 		
+		// Session
 		session = MockURLSession()
 		networkModel = NetworkModel(base: PixabayNetworkServiceUrl.apiBase,
 									path: PixabayNetworkServiceUrl.apiPath,
@@ -32,12 +34,23 @@ class HitsViewModelTests: XCTestCase {
 									method: .get)
 		networkManager = NetworkManager(networkModel: networkModel, session: session)
 		
+		// Services
 		pixaBayNetworkService = PixabayNetworkService(networkManager: networkManager)
-		
 		hitsIterator = HitsInteractor(pixaBayNetworkService)
 		coordinator = MockCoordinator()
 		
 		hitsViewModel = HitsViewModel(interactor: hitsIterator, coordinator: coordinator)
+		generateMockResponseModel()
+	}
+	
+	private func generateMockResponseModel() {
+		guard let ressourceURL = Bundle(for: type(of: self)).url(forResource: "PixabayHitsResponseModel", withExtension: "json"),
+			let resourceData = try? Data(contentsOf: ressourceURL) else {
+				XCTFail("PixabayHitsResponseModel.json file doesn't exist or it's invalid")
+				return
+		}
+		
+		mockJson = resourceData
 	}
 	
 	override func tearDown() {
@@ -48,12 +61,23 @@ class HitsViewModelTests: XCTestCase {
 		
 		pixaBayNetworkService = nil
 		hitsIterator = nil
+		mockJson = nil
 		coordinator = nil
 		hitsViewModel = nil
 	}
 	
-	func test_fetchHits_withData() {
-		
+	func test_numberOfItems_withData_shouldHave3Items() {
+		session.nextData = mockJson
+		hitsViewModel.fetchHits()
+		XCTAssertEqual(hitsViewModel.numberOfItems(), 3, "The hits model doesn't have 3 items")
+	}
+	
+	func test_numberOfItems_withNoData_shouldNotHaveItems() {
+		hitsViewModel.fetchHits()
+		XCTAssertEqual(hitsViewModel.numberOfItems(), 0, "The hits model doesn't have 0 items")
+	}
+	func test_title_shouldBePixabayHits() {
+		XCTAssertEqual(hitsViewModel.title, "Pixabay Hits", "The title is not Pixabay Hits")
 	}
 }
 
